cac2c850d9b52f9053c180fd318fe6f792ab07ac
Wenshuo7 <130787053+Wenshuo7@users.noreply.github.com>
2023-04-15 16:08:18 +0200

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -3,11 +3,11 @@ import { Knex, knex } from "knex";
 const config: Knex.Config = {
   client: "mysql",
   connection: {
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     port: 3306 ,
-    user: "root",
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "greenhouse",
+    database: process.env.DB_DATABASE,
   },
 };
 
