dc055f17160585666adb40fb2e30533bee144320
Wenshuo7 <130787053+Wenshuo7@users.noreply.github.com>
2023-04-15 14:34:20 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ app.use(cors());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://greenhouse-frontend.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
